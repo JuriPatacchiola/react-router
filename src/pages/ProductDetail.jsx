2249f820acf79911c03ebcdc4139ade2331f351b
JuriPatacchiola <jpatacchiola7@gmail.com>
2025-11-17 12:40:44 +0100

    add the import useeffect , usestate and useparams in product detail and add the function th two const and the fetch for to take the informaaction in the link api and the id posiction

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -1,6 +1,15 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
-function ProductDetail() {
+export default function ProductDetail() {
+    const { id } = useParams();
+    const [product, setProduct] = useState(null);
 
+    useEffect(() => {
+        fetch(`https://fakestoreapi.com/products/${id}`)
+            .then((res) => res.json())
+            .then((data) => setProduct(data));
+    }, [id]);
 
     return (
         <>
@@ -9,4 +18,4 @@ function ProductDetail() {
     );
 }
 
-export default ProductDetail;
+
