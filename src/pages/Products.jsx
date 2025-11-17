7cf00dae0e943fd60ae88c27568938bd3afe9a2e
JuriPatacchiola <jpatacchiola7@gmail.com>
2025-11-17 12:08:29 +0100

    add the import and use stae and effect for to see the element in the api link

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,14 +1,18 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
+export default function Products() {
+    const [products, setProducts] = useState([]);
+    const navigate = useNavigate();
 
-function Products() {
-
-
+    useEffect(() => {
+        fetch("https://fakestoreapi.com/products")
+            .then((res) => res.json())
+            .then((data) => setProducts(data));
+    }, []);
     return (
-
         <>
-
         </>
-    );
+    )
 }
 
-export default Products;
