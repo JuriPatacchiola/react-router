9609d53c67e9351e95131b4974deb4b2988d2676
JuriPatacchiola <jpatacchiola7@gmail.com>
2025-11-17 12:02:22 +0100

    add the link for to navigate the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,11 @@
+import { Link } from "react-router-dom";
 
 export default function Header() {
-
     return (
-        <>
-
-
-        </>
-    )
+        <nav className="navbar">
+            <Link to="/">Home</Link>
+            <Link to="/chi-siamo">Chi Siamo</Link>
+            <Link to="/prodotti">Prodotti</Link>
+        </nav>
+    );
 }
-
