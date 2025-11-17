2409761125538713776016c0f6d11e2297a42489
JuriPatacchiola <jpatacchiola7@gmail.com>
2025-11-17 12:04:44 +0100

    add the import tre links and add the route links

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
+import { Route, Routes } from "react-router-dom";
+
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Main from "./components/Main";
+
+import ChiSiamo from "./pages/ChiSiamo";
+import HomePage from "./pages/HomePage";
+import Products from "./pages/Products";
 
 function App() {
   return (
     <>
       <Header />
-
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/chi-siamo" element={<ChiSiamo />} />
+        <Route path="/prodotti" element={<Products />} />
+        <Route path="/prodotti/:id" element={<ProductDetail />} />
+      </Routes>
       <Main />
 
       <Footer />
